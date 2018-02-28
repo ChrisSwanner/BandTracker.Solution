192c84c4faf836664d2ee5b7d19955c54ecb591e
Chris Swanner <cmanswanner@gmail.com>
2018-02-28 15:30:40 -0800

    Initial commit

---
+++ BandTracker.Test/ModelTests/VenueTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using BandTracker.Models;
+using System;
+
+namespace Bandtracker.TestTools
+{
+  [TestClass]
+  public class VenueTests : IDisposable
+  {
+
+  }
+}
